c62c6acfd916de9fd5752be0d827d5ccd2580ebe
Vladimir218 <kvv23@tut.by>
2023-04-07 20:39:23 +0300

    Урок1. Задание 2

--- lesson1/lesson1.py
+++ lesson1/lesson1.py
@@ -19,3 +19,26 @@ with open('Internet-data/Internet-data/lesson1/task1.json', 'w') as f:
 
 for i in r.json():
     print(f"{i['id']} {i['name']}")
+
+
+# Task2
+# Изучить список открытых API (https://www.programmableweb.com/category/all/apis).
+# Найти среди них любое, требующее авторизацию (любого типа). Выполнить запросы к нему,
+# пройдя авторизацию. Ответ сервера записать в файл.
+# Если нет желания заморачиваться с поиском, возьмите API вконтакте
+# (https://vk.com/dev/first_guide). Сделайте запрос, чтобы получить
+# список всех сообществ на которые вы подписаны.
+
+
+url = 'https://api.github.com/user/emails'
+token = input('Введите токен доступа: ')
+
+headers = {
+    'Authorization': f'Bearer {token}'
+}
+
+email_information = requests.get(f'{url}', headers = headers)
+print(email_information.json())
+
+with open('Internet-data/Internet-data/lesson1/task2.json', 'w') as f:
+    json.dump(email_information.json(), f)
